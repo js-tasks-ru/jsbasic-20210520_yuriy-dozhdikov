095cf3890477c1b91bf89e94a9c6900e64a5ce5a
Yuri Dozhdikov <yuri.dozhdikov@gmail.com>
2021-06-04 11:16:11 +0300

    решение 1 задачи 4 модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
+const { create } = require("eslint/lib/rules/*");
+
 function makeFriendsList(friends) {
-  
+  let ul = document.createElement('ul');
+  for (let i = 0; i < friends.length; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = friends[i].firstName + ' ' + friends[i].lastName;
+    ul.appendChild(li);
+  }
+  return ul;
 }
